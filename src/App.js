0e6d7890d53962b78d7de2eb9ecdc55108627691
Luana <luanateixeira148@gmail.com>
2022-01-03 19:17:47 -0800

    styling setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello my portfolio</h1>
+      <h1 className="test-title">Hello my portfolio</h1>
     </div>
   );
 }
