337fe120d2dc87e0fb538f10c76f8a425936f470
luanateixeira148 <luanateixeira148@gmail.com>
2022-01-06 15:26:45 -0800

    create button component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.scss';
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="test-title">Hello, world</h1>
-      <h1>H1 Almost before we knew it, we had left the ground.</h1>
-      <h2>H2 Almost before we knew it, we had left the ground.</h2>
-      <h3>H3 Almost before we knew it, we had left the ground.</h3>
-      <h4>H4 Almost before we knew it, we had left the ground.</h4>
-      <h5>H5 Almost before we knew it, we had left the ground.</h5>
-      <p>P Almost before we knew it, we had left the ground.</p>
+      
+      <Button 
+        text={"SEE MORE"}
+      />
+      
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import "./button.scss";
+
+function Button(props) {
+
+  return (
+    <button className="button-3d">{props.text}</button>
+  );
+
+}
+
+export default Button;
