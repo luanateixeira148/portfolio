4676e0e9b4824c3feb31e9272277706f87d3559a
Luana <49740425+luanateixeira148@users.noreply.github.com>
2022-01-10 17:56:38 -0800

    create about component (#5)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
 import ProjectsComp from './components/Projects';
 import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <Home />
       <ProjectsComp />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,33 @@
+import "./about.scss";
+import Button from "./Button"
+
+function About () {
+
+  return (
+    <div className="about">
+      <header>
+        <h3>&lt; about &gt;</h3>
+      </header>
+      <main>
+        <div className="profile-photo">
+          Profile photo
+        </div>
+        <div className="bio">
+          <p>
+            With a passion for learning, I’m fascinated with Human-Computer Interactions and user-centred design.
+            I have a strong background in UI/UX Design, though my expertise lies in simple yet effective web and software development.
+          </p>
+        </div>
+        <div className="about-button">
+          <Button text={"RESUME (PDF)"} />
+        </div>
+      </main>
+      <footer>
+        <h3>&lt;/ about &gt;</h3>
+      </footer>
+    </div>
+  );
+
+}
+
+export default About;
