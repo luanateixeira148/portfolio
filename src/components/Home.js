cb126641889dfdb61073c9dd59880427c1e62fe8
Luana <luanateixeira148@gmail.com>
2022-02-01 18:33:36 -0800

    change layout on main component

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,16 +6,14 @@ function Home () {
 
   return (
     <div className="home">
-      <Header />
       <main>
         <div className="intro">
           <h4 className="yellow">HELLO, WORLD!</h4>
           <h1 className="off-white">I am Luana</h1>
-          <p className="off-white">A full stack developer <br />
-              located in Vancouver.</p>
+          <p className="off-white">A full stack developer located in Vancouver.</p>
         </div>
         <div className="illustration">
-          <div></div>
+          <img src={logo} className="logo"/>
         </div>
       </main>
     </div>
