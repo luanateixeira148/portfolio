6809e3dc00e4f8fb769d30bcedef73b05eaf7340
Luana <luanateixeira148@gmail.com>
2022-02-03 19:42:22 -0800

    style home comp for desktop and mobile

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,19 @@
 import "./home.scss";
-import logo from "../assets/logo.svg";
+import Header from './Header';
 
 function Home () {
 
   return (
     <div className="home">
+      <header>
+        <Header />
+      </header>
       <main>
         <div className="intro">
-          <h4 className="yellow">HELLO, WORLD!</h4>
-          <h1 className="off-white">I am Luana</h1>
-          <p className="off-white">A full stack developer located in Vancouver.</p>
-        </div>
-        <div className="illustration">
-          <img src={logo} className="logo"/>
+          <h1 className="upper-title yellow">HELLO, WORLD!</h1>
+          <h1 className="title-name off-white">I am Luana</h1>
+          <p className="subtitle off-white">
+            A <span>full stack developer</span> based in Vancouver, Canada.</p>
         </div>
       </main>
     </div>
