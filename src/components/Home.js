5dcbfb4145c14f9fdecd16285c46aca2e2d8086c
Luana <luanateixeira148@gmail.com>
2022-02-04 14:20:01 -0800

    add highlight on home subtitle

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,7 @@ function Home () {
           <h1 className="upper-title yellow">HELLO, WORLD!</h1>
           <h1 className="title-name off-white">I am Luana</h1>
           <p className="subtitle off-white">
-            A <span>full stack developer</span> based in Vancouver, Canada.</p>
+            A <mark>full stack developer</mark> based in Vancouver, Canada.</p>
         </div>
       </main>
     </div>
