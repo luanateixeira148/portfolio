0188d40ae2632e0c568118ac180760b6e0117a0d
Luana <luanateixeira148@gmail.com>
2022-02-24 09:30:13 -0800

    increase font-size for mobile

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -12,7 +12,7 @@ function About () {
       <main>
         <div className="bio">
           <p>
-            Borned and raised in Brazil, I moved to Vancouver in 2017. Since then, life has been an adventure! I had to reinvent myself professionally, and that's how I discovered my interest for the <span>digital world</span>.
+            Born and raised in Brazil, I moved to Vancouver in 2017. Since then, life has been an adventure! I had to reinvent myself professionally, and that's how I discovered my interest for the <span>digital world</span>.
           </p>
           <p>
             With a passion for learning, I am fascinated with Human-Computer Interactions. I thrive when I can use my coding skills to create beautiful and intuitive user experiences. I have a background in UI/UX Design, though my expertise lies in simple yet effective <span>web and mobile development</span>.
