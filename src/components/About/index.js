9f1ab83e733d40ca4bed9912ffba5a1f83ebe908
Luana <luanateixeira148@gmail.com>
2022-03-10 10:14:17 -0800

    redesign about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,30 +6,28 @@ function About () {
   
   return (
     <div className="about" id="about">
-      <SectionTags 
-        text={"< about >"}
-      />
       <main>
         <div className="bio">
+          <h1 className="title">&lt; about &gt;</h1>
           <p>
-            Born and raised in Brazil, I moved to Vancouver in 2017. Since then, life has been an adventure! I had to reinvent myself professionally, and that's how I discovered my interest for the <span>digital world</span>.
+            Born and raised in Brazil, I moved to Vancouver in 2017. Since then, life has been an adventure! I had to reinvent myself professionally, and that's how I discovered my interest for the digital world.
           </p>
           <p>
-            With a passion for learning, I am fascinated with Human-Computer Interactions. I thrive when I can use my coding skills to create beautiful and intuitive user experiences. I have a background in UI/UX Design, though my expertise lies in simple yet effective <span>web and mobile development</span>.
+            With a passion for learning, I am fascinated with Human-Computer Interactions. I thrive when I can use my coding skills to create beautiful and intuitive user experiences. I have a background in UI/UX Design, though my expertise lies in simple yet effective web and mobile development.
           </p>
-          <div className="about-button">
-            <a href={process.env.PUBLIC_URL + "/resume.pdf"} target="_blank" type="application/pdf">
-              <PrimaryButton text={"RESUME (PDF)"} />
-            </a>
-          </div>
+          <a 
+            href={process.env.PUBLIC_URL + "/resume.pdf"} 
+            target="_blank" 
+            type="application/pdf"
+            className="resume-link"
+          >
+            <PrimaryButton text={"RESUME (PDF)"} />
+          </a>
         </div>
         <img 
-          src={process.env.PUBLIC_URL + "/images/luana.png"} className="profile-photo"
+          src={process.env.PUBLIC_URL + "/images/about-photo.png"} className="profile-photo"
         />
       </main>
-      <SectionTags 
-        text={"</ about >"}
-      />
     </div>
   );
 
