4d71b61c670bf0f03fa32595471cae67428bfb91
Luana <luanateixeira148@gmail.com>
2022-03-09 17:04:25 -0800

    fix navbar useState

--- src/components/NavBar/SideNav/index.js
+++ src/components/NavBar/SideNav/index.js
@@ -19,22 +19,22 @@ function SideNav(props) {
         <Link 
           to="/portfolio/projects" 
           className="side-nav-link" 
-          onClick={() => props.setMenuOpen(false)}
+          onClick={() => props.setBurgerMenuOpen(false)}
           >
           <h2>Projects</h2>
         </Link>
         <HashLink 
           to="/portfolio#about" 
           className="side-nav-link" 
-          onClick={() => props.setMenuOpen(false)}
+          onClick={() => props.setBurgerMenuOpen(false)}
           >
           <h2>About</h2>
         </HashLink>
         <HashLink 
           to="/portfolio#contact" 
           className="side-nav-link" 
-          onClick={() => props.setMenuOpen(false)}
-          >
+          onClick={() => props.setBurgerMenuOpen(false)}
+        >
           <h2>Contact</h2>
         </HashLink>
         <a 
@@ -42,7 +42,7 @@ function SideNav(props) {
           target="_blank" 
           type="application/pdf"
           className="side-nav-link" 
-          onClick={() => props.setMenuOpen(false)}
+          onClick={() => props.setBurgerMenuOpen(false)}
         >
           <h2>Resume</h2>
         </a>
